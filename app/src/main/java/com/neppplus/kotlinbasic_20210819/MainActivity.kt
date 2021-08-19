4641276926e74ba51209fe6c8489803053d86e08
이재환 <qorvhfl080@naver.com>
2021-08-19 10:09:42 +0900

    로그 사용

--- app/src/main/java/com/neppplus/kotlinbasic_20210819/MainActivity.kt
+++ app/src/main/java/com/neppplus/kotlinbasic_20210819/MainActivity.kt
@@ -2,6 +2,9 @@ package com.neppplus.kotlinbasic_20210819
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
 import com.neppplus.kotlinbasic_20210819.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -10,6 +13,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        logBtn01.setOnClickListener {
+
+            Log.d("메인화면", "Log.debug")
+            Log.e("메인화면", "Log.error")
+            Log.i("메인화면", "Log.info")
+
+        }
 
     }
 }
